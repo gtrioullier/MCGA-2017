16797fb905acd58022771658a90d7b23c85bde21
gtrioullier <gustavo.trioullier@alumnos.uai.edu.ar>
2017-09-02 20:47:49 -0300

    ABM Categoria v 0

---
+++ SolutionsLeatherGoods/Presentation/ASF.UI.Process/CategoryProcess.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using ASF.Entities;
+using ASF.Services.Contracts;
+using ASF.UI.Process;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace ASF.UI.Process
+{
+    public class CategoryProcess : ProcessComponent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Category> SelectList()
+        {
+            var response = HttpGet<AllResponse>("rest/Category/All", new Dictionary<string, object>(), MediaType.Json);
+            return response.Result;
+        }
+
+        public Category Find(int id)
+        {
+            var response = HttpGet<FindResponse>("rest/Category/Find/"+id, new Dictionary<string, object>(), MediaType.Json);
+            return response.Result;
+        }
+
+        public void Create(Category C)
+        {
+            var request = HttpPost<Category>("rest/Category/Add", C, MediaType.Json);
+        }
+
+        public void Delete(int id)//Service es remove{id}
+        {
+            var response = HttpGet<FindResponse>("rest/Category/Remove/"+id, new Dictionary<string, object>(), MediaType.Json);
+        }
+
+        public void Edit(Category C)//Service es edit{category}
+        {
+            var request = HttpPost<Category>("rest/Category/Edit/", C, MediaType.Json);//cambié HttpPut por HttpPost
+        }
+    }
+}
---
+++ SolutionsLeatherGoods/Presentation/ASF.UI.WbSite/Areas/Categories/Controllers/CategoryController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASF.UI.WbSite.Areas.Categories.Controllers
+{
+
+    public class CategoryController : Controller
+    {
+        // GET: Categories/Category
+        public ActionResult Index()
+        {
+            var cp = new ASF.UI.Process.CategoryProcess();
+            var lista = cp.SelectList();
+            return View(lista);
+        }
+
+        //GET: Categories/Category/Details/5
+        public ActionResult Details(int id)
+        {
+            var cp = new ASF.UI.Process.CategoryProcess();
+            var categoria = cp.Find(id);
+            return View(categoria);
+        }
+
+        //GET: Categories/Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Categories/Category/Create
+        [HttpPost]
+        public ActionResult Create(ASF.Entities.Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                var cp = new ASF.UI.Process.CategoryProcess();
+                model.CreatedOn = DateTime.Now;
+                model.ChangedOn = DateTime.Now;
+                cp.Create(model);
+            }
+            return RedirectToAction("Index");
+        }
+
+        //GET: Categories/Category/Delete
+        public ActionResult Delete(int id)
+        {
+            var cp = new ASF.UI.Process.CategoryProcess();
+            var categoria = cp.Find(id);
+            return View(categoria);
+        }
+
+        //POST: Categories/Category/Delete
+        [HttpPost]
+        public ActionResult Delete(ASF.Entities.Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                var cp = new ASF.UI.Process.CategoryProcess();
+                cp.Delete(model.Id);
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        //GET: Categories/Category/Edit
+        public ActionResult Edit(int id)
+        {
+            var cp = new ASF.UI.Process.CategoryProcess();
+            var categoria = cp.Find(id);
+            return View(categoria);
+        }
+
+        //POST: Categories/Category/Edit
+        [HttpPost]
+        public ActionResult Edit(ASF.Entities.Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                var cp = new ASF.UI.Process.CategoryProcess();
+                model.ChangedOn = DateTime.Now;
+                if (model.CreatedBy == 0)
+                {
+                    model.CreatedBy = null;
+                }
+                cp.Edit(model);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
