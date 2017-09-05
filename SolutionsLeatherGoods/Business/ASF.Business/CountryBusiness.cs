bdcb76df126c1db535de77ae84eb123f1d63d376
gtrioullier <gustavo.trioullier@alumnos.uai.edu.ar>
2017-09-05 19:37:38 -0300

    insertar country

---
+++ SolutionsLeatherGoods/Business/ASF.Business/CountryBusiness.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using ASF.Entities;
+using ASF.Data;
+
+namespace ASF.Business
+{
+    public class CountryBusiness
+    {
+        public List<Country> All()
+        {
+            var countryDac = new CountryDAC();
+            var result = countryDac.Select();
+            return result;
+        }
+
+        public Country Find(int id)
+        {
+            var countryDac = new CountryDAC();
+            var result = countryDac.SelectById(id);
+            return result;
+        }
+
+        public Country Add(Country country)
+        {
+            var countryDac = new CountryDAC();
+            return countryDac.Create(country);
+        }
+
+        public void Remove(int id)
+        {
+            var countryDac = new CountryDAC();
+            countryDac.DeleteById(id);
+        }
+
+        public void Edit(Country country)
+        {
+            var countryDac = new CountryDAC();
+            countryDac.UpdateById(country);
+        }
+    }
+}
