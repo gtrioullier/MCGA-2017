c05830d57ec64871812db415c974a38f8035d742
gtrioullier <gustavo.trioullier@alumnos.uai.edu.ar>
2017-09-05 23:14:56 -0300

    ABM Categoria v 0.1

---
+++ SolutionsLeatherGoods/Business/ASF.Business/CountryBusiness.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using ASF.Entities;
+using ASF.Data;
+
+
+namespace ASF.Business
+{
+
+    public class CountryBusiness
+    {
+
+        public Country Add(Country country)
+        {
+            var countryDac = new CountryDAC();
+            return countryDac.Create(country);
+        }
+
+        public void Remove(int id)
+        {
+            var countryDac = new CountryDAC();
+            countryDac.DeleteById(id);
+        }
+
+        public List<Country> All()
+        {
+            var countryDac = new CountryDAC();
+            var result = countryDac.Select();
+            return result;
+        }
+
+        public Country Find(int id)
+        {
+            var countryDac = new CountryDAC();
+            var result = countryDac.SelectById(id);
+            return result;
+        }
+
+        public void Edit(Country country)
+        {
+            var countryDac = new CountryDAC();
+            countryDac.UpdateById(country);
+        }
+    }
+}
